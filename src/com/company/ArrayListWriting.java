02b566f581bc109a11d084d18047aa6516351f42
SriramG1 <sriramhht@gmail.com>
2022-03-17 16:03:15 +0530

    'rename file'

--- src/com/company/ArrayListWriting.java
+++ src/com/company/ArrayListWriting.java
@@ -3,8 +3,8 @@ package com.company;
 import java.io.*;
 import java.util.ArrayList;
 
-public class Sample {
-    public static void main(String[] args) throws IOException,ClassNotFoundException {
+public class ArrayListWriting {
+    public static void main(String[] args) throws IOException {
         FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\sys\\Desktop\\File\\Registration.txt");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         ArrayList<Details> array = new ArrayList<>();
